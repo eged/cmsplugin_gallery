8e5ad55acb4c43c20feef58c905c42f880c8c34a
Viliam Segeda <viliam.segeda@gmail.com>
2011-04-14 23:50:28 +0200

    converting models for django-filer

--- cmsplugin_gallery/models.py
+++ cmsplugin_gallery/models.py
@@ -1,6 +1,7 @@
 from cms.models import CMSPlugin
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from filer.fields.image import FilerImageField
 from inline_ordering.models import Orderable
 import utils
 
@@ -13,21 +14,31 @@ class GalleryPlugin(CMSPlugin):
                                 choices=TEMPLATE_CHOICES, 
                                 default='cmsplugin_gallery/gallery.html',
                                 editable=len(TEMPLATE_CHOICES) > 1)
+    src_width = models.PositiveSmallIntegerField(editable=False, null=True)
+    src_height = models.PositiveSmallIntegerField(editable=False, null=True)
     
     def __unicode__(self):
         return _(u'%(count)d image(s) in gallery') % {'count': self.image_set.count()}
 
-
+    def get_size(self):
+        """ returns given or found width x height tuple"""
+        size = (0, 0)
+        if self.src_width:
+            size = (self.src_width, self.src_height)
+        else:
+            if self.image_set.count() > 0:
+                img = self.image_set.all()[0]
+                size = (img.src.width, img.src.height) 
+        return size 
+    
 class Image(Orderable):
     
     gallery = models.ForeignKey(GalleryPlugin)
-    src = models.ImageField(upload_to='cmsplugin_gallery/images', 
-                            height_field='src_height', 
-                            width_field='src_width')
-    src_height = models.PositiveSmallIntegerField(editable=False, null=True)
-    src_width = models.PositiveSmallIntegerField(editable=False, null=True)
+    src = FilerImageField()
     title = models.CharField(max_length=255, blank=True)
     alt = models.TextField(blank=True)
     
     def __unicode__(self):
         return self.title or self.alt or str(self.pk)
+    
+    
